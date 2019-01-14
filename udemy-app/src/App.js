73e647fa35d2a79ed52610fdf82df73f77eedb18
Mikhaël Bois <mikhaelange@gmail.com>
2019-01-13 20:33:44 -0500

    Udemy React - Completed Section 3

--- udemy-app/src/App.js
+++ udemy-app/src/App.js
@@ -1,28 +1,103 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        persons: [
+            {
+                name:"Bob",
+                age:"99"
+            },
+            {
+                name:"Job",
+                age:"19"
+            },
+            {
+                name:"Mob",
+                age:"9"
+            }
+        ]
+    }
+
+    switchNameHandler = (newName) => {
+        this.setState({
+            persons: [
+                {
+                    name:newName,
+                    age:"1119"
+                },
+                {
+                    name:"Job",
+                    age:"19"
+                },
+                {
+                    name:"Mobydick",
+                    age:"9"
+                }
+            ]
+        });
+    }
+
+    nameChangeHandler = (event) => {
+        this.setState({
+            persons: [
+                {
+                    name:"Bob",
+                    age:"1119"
+                },
+                {
+                    name: event.target.value,
+                    age:"19"
+                },
+                {
+                    name:"Mob",
+                    age:"9"
+                }
+            ]
+        });
+    }
+
+    render() {
+        const style = {
+            backgroundColor: '#333',
+            border: 'none',
+            color: '#fff',
+            padding: '1em',
+            cursor: 'pointer'
+        };
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Udemy App</h1>
+                </header>
+                <p>
+                    This is a test.
+                </p>
+                <button
+                    onClick={() => this.switchNameHandler('Max1')}
+                    style={style} >
+                    Switchy time!
+                </button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}
+                    click={this.switchNameHandler.bind(this, 'Max2')} />
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    change={this.nameChangeHandler} >
+                    <h5>My hobbies</h5>
+                </Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}/>
+            </div>
+        );
+
+        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Udemy App'));
+    }
 }
 
 export default App;
---
+++ udemy-app/src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Person.css';
+
+const person = (props) => {
+    return (
+        <div className="Person" onClick={props.click}>
+            <h3>I'm {props.name} and I wish I was {props.age} years old again!</h3>
+            {props.children}
+            <input type="text" onChange={props.change} value={props.name} />
+        </div>
+    );
+}
+
+export default person;
